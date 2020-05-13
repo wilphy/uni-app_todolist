c9f33037292a6f208ccafc6a1864fa3fee15d488
wilphy <149201216@qq.com>
2020-05-13 22:38:14 +0800

    内容卡片布局

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -14,6 +14,20 @@
 				<view class="todo-header__right-item">已完成</view>
 			</view>
 		</view>
+
+		<!-- 内容 -->
+		<view class="todo-content">
+			<view class="todo-list todo--finsh">
+				<view class="todo-list__checkbox"><view class="checkbox"></view></view>
+				<view class="todo-list__content">我的待办事项</view>
+			</view>
+		</view>
+		<view class="todo-content">
+			<view class="todo-list">
+				<view class="todo-list__checkbox"><view class="checkbox"></view></view>
+				<view class="todo-list__content">我的待办事项</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -29,9 +43,9 @@ export default {
 
 <style>
 .todo-header {
-	position: fixed;
+	/* position: fixed;
 	top: 0;
-	left: 0;
+	left: 0; */
 	display: flex;
 	align-items: center;
 	padding: 0 15px;
@@ -42,7 +56,7 @@ export default {
 	box-sizing: border-box;
 	box-shadow: -1px 1px 5px 0 rgba(0, 0, 0, 0.1);
 	background: #ffffff;
-	z-index: 10;
+	/* z-index: 10; */
 }
 
 .todo-header__left {
@@ -66,5 +80,84 @@ export default {
 
 .active-tab {
 	color: #279abf;
+}
+
+.todo-content {
+	position: relative;
+}
+
+.todo-list {
+	position: relative;
+	display: flex;
+	align-items: center;
+	padding: 15px;
+	margin: 15px;
+	border-radius: 10px;
+	font-size: 14px;
+	color: #0c3854;
+	background-color: #abcdef;
+	box-shadow: -1px 1px 5px 1px rgba(0, 0, 0, 0.1), -1px 2px 1px 0 rgba(255, 255, 255) inset;
+	overflow: hidden;
+}
+
+.todo-list:after {
+	position: absolute;
+	content: '';
+	top: 0;
+	bottom: 0;
+	left: 0;
+	width: 5px;
+	background-color: #ff0066;
+}
+
+.todo-list__checkbox {
+	padding-right: 15px;
+}
+
+.checkbox {
+	width: 20px;
+	height: 20px;
+	border-radius: 50%;
+	background-color: #ffffff;
+	box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.1);
+}
+
+.todo--finsh .checkbox {
+	position: relative;
+	background-color: #eee;
+}
+.todo--finsh .checkbox:after {
+	content: '';
+	position: absolute;
+	width: 10px;
+	height: 10px;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	margin: auto;
+	background-color: #cfebfd;
+	border-radius: 50%;
+	box-shadow: 0 0 2px 0px rgba(0, 0, 0, 0.2) inset;
+}
+
+.todo--finsh .todo-list__content {
+	color: #999;
+}
+
+.todo--finsh.todo-list:before {
+	content: '';
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 40px;
+	right: 10px;
+	height: 2px;
+	margin: auto 0;
+	background-color: #aaa;
+}
+
+.todo--finsh.todo-list:after {
+	background-color: #ccc;
 }
 </style>
